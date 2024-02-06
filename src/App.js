43431f503ae9d95144026727d389aae3a961dbfd
ZinKoLin <zinkolin113@gmail.com>
2024-02-06 13:16:26 +0630

    fetch todoList & line-though Conditional class

--- src/App.js
+++ src/App.js
@@ -5,19 +5,29 @@ import TodoList from './components/TodoList';
 import CheckAllAndRemain from './components/CheckAllAndRemain';
 import TodoFilter from './components/TodoFilter';
 import ClearCompleteBtn from './components/ClearCompleteBtn';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  let[todos,setTodos] =useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:3001/todo')
+    .then(res=>res.json())
+    .then((todo)=>{
+      
+      setTodos(todo)
+      
+    })
+
+  },[])
   return (
     <div className="todo-app-container">
       <div className="todo-app">
         <h2>Todo App</h2>
 
         <TodoForm/>
-
-
- 
-        <TodoList/>
-
+        <TodoList todos={todos}/>
         <CheckAllAndRemain/>
 
       
